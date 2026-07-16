6ca5d0d05622ef88520f4ba3684f5aa51b7e2ae3
vinicius-dev-data-scientist <vinicius.dev.tech@gmail.com>
2026-07-16 14:30:36 -0400

    fix: Verificando caminhos de arquivo

--- pages/inmet_dash_plot.py
+++ pages/inmet_dash_plot.py
@@ -16,7 +16,6 @@ from datetime import timedelta
 # =========================
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 DATASET_DIR = os.path.join(BASE_DIR, "datasets", "inmet")
 
 # =========================
